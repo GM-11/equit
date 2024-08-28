3883068bf2d477402fd2e05f482948017cdb15a0
GM-11 <mathuyrgopal@gmail.com>
2024-08-28 20:01:17 +0000

    added react router

---
+++ frontend/src/screens/LandingPage.tsx
@@ -0,0 +1,14 @@
+function LandingPage() {
+  return (
+    <main>
+      <h1>EQUIT</h1>
+
+      <h2>
+        Manage your equity on <br />
+        BITCOIN
+      </h2>
+    </main>
+  );
+}
+
+export default LandingPage;
