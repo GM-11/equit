aba4b994a4d81b8940131bd4303a80b6d1b0d5b1
GM-11 <mathuyrgopal@gmail.com>
2024-08-30 19:28:46 +0000

    added company path

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import LandingPage from "./screens/LandingPage.tsx";
 import CreateCompany from "./screens/CreateCompany.tsx";
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
 import AllCompanies from "./screens/AllCompanies.tsx";
+import CompanyDashboard from "./screens/CompanyDashboard.tsx";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
         path: "/companies",
         element: <AllCompanies />,
       },
+      {
+        path: "/company/:tokenAddress",
+        element: <CompanyDashboard/>
+      },
     ],
   },
 ]);
@@ -31,17 +36,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")!).render(<Root />);
 
 function Root() {
-  // 1. Your WalletConnect Cloud project ID
   const projectId = "7594a316c1c467100dafe6fe766c3914";
-
-  // 2. Set chains
-  const mainnet = {
-    chainId: 1,
-    name: "Ethereum",
-    currency: "ETH",
-    explorerUrl: "https://etherscan.io",
-    rpcUrl: "https://cloudflare-eth.com",
-  };
 
   const anvil = {
     chainId: 31337,
@@ -51,38 +46,49 @@ function Root() {
     rpcUrl: "http://127.0.0.1:8545",
   };
 
-  // 3. Create a metadata object
+  const baseSepolia = {
+    chainId: 84532,
+    name: "Base Sepolia",
+    currency: "ETH",
+    rpcUrl: "https://chain-proxy.wallet.coinbase.com?targetName=base-sepolia",
+    explorerUrl: "https://basescan.org/",
+  };
+
+  const coreTestnet = {
+    chainId: 1115,
+    name: "Core Blockchain TestNet",
+    currency: "tCORE",
+    rpcUrl: "https://scan.test.btcs.network/",
+    explorerUrl: "https://rpc.test.btcs.network",
+  };
+
   const metadata = {
     name: "EQUIT",
     description: "AppKit Example",
-    url: "https://web3modal.com", // origin must match your domain & subdomain
+    url: "https://web3modal.com",
     icons: ["https://avatars.githubusercontent.com/u/37784886"],
   };
 
-  // 4. Create Ethers config
   const ethersConfig = defaultConfig({
-    /*Required*/
     metadata,
 
-    /*Optional*/
-    enableEIP6963: true, // true by default
-    enableInjected: true, // true by default
-    enableCoinbase: true, // true by default
-    rpcUrl: "...", // used for the Coinbase SDK
-    defaultChainId: 1, // used for the Coinbase SDK
+    enableEIP6963: true,
+    enableInjected: true,
+    enableCoinbase: true,
+    rpcUrl: "...",
+    defaultChainId: 1,
   });
 
-  // 5. Create a Web3Modal instance
   createWeb3Modal({
     ethersConfig,
     themeMode: "dark",
     themeVariables: {
       "--w3m-color-mix": "#708090",
     },
-    defaultChain: anvil,
-    chains: [mainnet, anvil],
+    defaultChain: baseSepolia,
+    chains: [coreTestnet, baseSepolia, anvil],
     projectId,
-    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    enableAnalytics: true,
   });
 
   return (
