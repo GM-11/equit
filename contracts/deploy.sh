5b514deace03d49eac3af0f28beaff24cc27ba2e
GM-11 <mathuyrgopal@gmail.com>
2024-08-30 19:26:01 +0000

    added more networks

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -1,16 +1,26 @@
 
-read -p "Enter network (local / prod): " network
+read -p "Enter network: " network
 
 if [ "$network" == "local" ]; then
 echo "Deploying Company Factory Contract"
 forge create ./src/CompanyTokenFactory.sol:CompanyTokenFactory --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+fi
 
-echo ""
-else
-    echo "Invalid network type. Please enter 'local' or 'prod'."
+if [ "$network" == "sepolia" ]; then
+echo "Deploy Company Factory Contract to Sepolia"
+forge create ./src/CompanyTokenFactory.sol:CompanyTokenFactory --rpc-url https://sepolia-rollup.arbitrum.io/rpc --private-key 0x7f3ed3d4b4d6dca6127fca65f465c94cd541938faf126d6c481fe9b8800e502b
+fi
+if [ "$network" == "base-sepolia" ]; then
+echo "Deploy Company Factory Contract to Sepolia"
+forge create ./src/CompanyTokenFactory.sol:CompanyTokenFactory --rpc-url https://chain-proxy.wallet.coinbase.com?targetName=base-sepolia --private-key 0x7f3ed3d4b4d6dca6127fca65f465c94cd541938faf126d6c481fe9b8800e502b
+fi
+
+if [ "$network" == "rootstock" ]; then
+echo "Deploy Company Factory Contract to Sepolia"
+forge create ./src/CompanyTokenFactory.sol:CompanyTokenFactory --rpc-url https://rpc.testnet.rootstock.io/UdkCPW4RQZD5wJE7bBEraFSkiWlTEk-T --private-key 0x7f3ed3d4b4d6dca6127fca65f465c94cd541938faf126d6c481fe9b8800e502b
 fi
 
 echo "Copying ABIS"
 cp -r out/CompanyToken.sol/CompanyToken.json ../frontend/src/utils/abis/CompanyToken.json
 cp -r out/CompanyTokenFactory.sol/CompanyTokenFactory.json ../frontend/src/utils/abis/CompanyTokenFactory.json
-echo "Copied"
+echo "Copying Done"
