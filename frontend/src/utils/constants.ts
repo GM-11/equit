fb8b5dba8b3e6133f50273eb05e17d9e1714cda7
GM-11 <mathuyrgopal@gmail.com>
2024-08-30 19:29:40 +0000

    updated contract

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,7 +2,7 @@ import CompanyToken from "./abis/CompanyToken.json";
 import CompanyNameFactory from "./abis/CompanyTokenFactory.json";
 
 export const COMPANY_TOKEN_FACTORY_CONTRACT_ADDRESS =
-  "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  "0xaA67B84b48C439c4d736369dF8d2284f3977CC56";
 
 export const COMPANY_TOKEN_FACTORY_CONTRACT_ABI = CompanyNameFactory.abi;
 
