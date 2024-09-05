4008e3ecb0bf58a6be0f8a578ad5ef0ac144226c
GM-11 <mathuyrgopal@gmail.com>
2024-09-05 13:19:24 +0000

    removed rootsstock

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -15,11 +15,6 @@ echo "Deploy Company Factory Contract to Sepolia"
 forge create ./src/CompanyTokenFactory.sol:CompanyTokenFactory --rpc-url https://chain-proxy.wallet.coinbase.com?targetName=base-sepolia --private-key 0x7f3ed3d4b4d6dca6127fca65f465c94cd541938faf126d6c481fe9b8800e502b
 fi
 
-if [ "$network" == "rootstock" ]; then
-echo "Deploy Company Factory Contract to Sepolia"
-forge create ./src/CompanyTokenFactory.sol:CompanyTokenFactory --rpc-url https://rpc.testnet.rootstock.io/UdkCPW4RQZD5wJE7bBEraFSkiWlTEk-T --private-key 0x7f3ed3d4b4d6dca6127fca65f465c94cd541938faf126d6c481fe9b8800e502b
-fi
-
 echo "Copying ABIS"
 cp -r out/CompanyToken.sol/CompanyToken.json ../frontend/src/utils/abis/CompanyToken.json
 cp -r out/CompanyTokenFactory.sol/CompanyTokenFactory.json ../frontend/src/utils/abis/CompanyTokenFactory.json
